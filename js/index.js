3f05771f5601b134cb1cedef447180ed81c4f7ab
Valentyn Malenchak <valentyn17@gmail.com>
2019-02-28 20:10:06 +0000

    Quadratic equation solution added

---
+++ js/index.js
@@ -0,0 +1,31 @@
+const a = parseFloat(prompt('Enter a value: '));
+const b = parseFloat(prompt('Enter b value: '));
+const c = parseFloat(prompt('Enter c value: '));
+
+const solution = solveQuadraticEquation(a, b, c);
+alert(solution);
+
+function solveQuadraticEquation(a, b, c)
+{
+    let discriminant = getDiscriminant(a, b, c);
+    if (discriminant >= 0)
+    {
+        let firstRoot = (-b + Math.sqrt(discriminant)) / (2 * a);
+        let secondRoot = (-b - Math.sqrt(discriminant)) / (2 * a);
+        return `First rational root is ${firstRoot}\n
+        Second rationalroot is ${secondRoot}`;
+    }
+    else
+    {
+        let RootRealPart = -b / (2 * a);
+        let FirstRootImaginePart = Math.sqrt(-discriminant) / (2 * a);
+        let SecondRootImaginePart = -Math.sqrt(-discriminant) / (2 * a);
+        return `First complex root is ${RootRealPart} + (${FirstRootImaginePart})*i\n
+        Second complex root is ${RootRealPart} + (${SecondRootImaginePart})*i`;
+    }
+}
+
+function getDiscriminant(a, b, c)
+{
+    return Math.pow(b, 2) - 4 * a * c;
+}
