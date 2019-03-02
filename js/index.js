8b6af33149450d586e3fd7e70a884c84dc65e83e
Valentyn Malenchak <valentyn17@gmail.com>
2019-03-02 09:46:08 +0000

    Semantic error fixed

--- js/index.js
+++ js/index.js
@@ -5,31 +5,26 @@ const c = parseFloat(prompt('Enter c value: '));
 const solution = solveQuadraticEquation(a, b, c);
 alert(solution);
 
-function solveQuadraticEquation(a, b, c)
-{
-    let discriminant = getDiscriminant(a, b, c);
-    if (discriminant > 0)
-    {
-        let firstRoot = (-b + Math.sqrt(discriminant)) / (2 * a);
-        let secondRoot = (-b - Math.sqrt(discriminant)) / (2 * a);
+function solveQuadraticEquation(a, b, c) {
+    const discriminant = getDiscriminant(a, b, c);
+    if (discriminant > 0) {
+        const firstRoot = (-b + Math.sqrt(discriminant)) / (2 * a);
+        const secondRoot = (-b - Math.sqrt(discriminant)) / (2 * a);
         return `First rational root is ${firstRoot}\n
         Second rational root is ${secondRoot}`;
     }
-    else if (discriminant === 0)
-    {
-        let root = -b / (2 * a);
+    else if (discriminant === 0) {
+        const root = -b / (2 * a);
         return `Rational root is ${root}`;
     }
-    else
-    {
-        let RootRealPart = -b / (2 * a);
-        let RootImaginePart = Math.sqrt(-discriminant) / (2 * a);
+    else {
+        const RootRealPart = -b / (2 * a);
+        const RootImaginePart = Math.sqrt(-discriminant) / (2 * a);
         return `First complex root is ${RootRealPart} + ${RootImaginePart} * i\n
         Second complex root is ${RootRealPart} - ${RootImaginePart} * i`;
     }
 }
 
-function getDiscriminant(a, b, c)
-{
-    return Math.pow(b, 2) - 4 * a * c;
+function getDiscriminant(a, b, c) {
+    return b ** 2 - 4 * a * c;
 }
